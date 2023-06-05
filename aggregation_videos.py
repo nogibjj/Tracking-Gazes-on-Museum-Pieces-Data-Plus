84fe2cb22b32a546d6a83c855ca48a018b479d98
EricR401S <eric.rios.3173@aol.com>
2023-06-05 16:52:07 -0400

    testing the end time for the analyses process

---
+++ aggregation_videos.py
@@ -0,0 +1,13 @@
+"""This script uses the video scripts to generate heatmaps"""
+import os
+import pandas as pd
+import cv2
+import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+### Import the function that will run the heatmap
+
+### In theory, the function will generate a heatmap
+# for each participant / per folder
