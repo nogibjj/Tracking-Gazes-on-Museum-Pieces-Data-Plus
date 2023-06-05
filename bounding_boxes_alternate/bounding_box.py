34bf1dd80a5b7ff9953c7fb6d6d27acd3679b2a3
EricR401S <eric.rios.3173@aol.com>
2023-06-05 13:48:14 -0400

    updated bounding box base - alternate

--- bounding_boxes_alternate/bounding_box.py
+++ bounding_boxes_alternate/bounding_box.py
@@ -10,13 +10,14 @@ import cv2
 import pandas as pd
 import datetime as dt
 
+print("we are looking at the tv")
 # path_0 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_30b\2022_30b\8bef8eba_0.0-63.584.mp4"
-path_0 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_03bm\2022_03bm\2e6f4a06_0.0-65.563.mp4"
-# path_0 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_39bm\2022_39bm\98b876d7_0.0-61.188.mp4"
+# path_0 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_03bm\2022_03bm\2e6f4a06_0.0-65.563.mp4"
+path_0 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_39bm\2022_39bm\98b876d7_0.0-61.188.mp4"
 
 # path_1 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_30b\2022_30b\gaze.csv"
-path_1 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_03bm\2022_03bm\gaze.csv"
-# path_1 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_39bm\2022_39bm\gaze.csv"
+# path_1 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_03bm\2022_03bm\gaze.csv"
+path_1 = r"C:\Users\ericr\Desktop\Data + Plus\Tracking-Gazes-on-Museum-Pieces-Data-Plus\2022_39bm\2022_39bm\gaze.csv"
 
 cap = cv2.VideoCapture(path_0)
 gaze = pd.read_csv(path_1)
