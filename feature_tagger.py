523d8d53f99ab5b23d2b5e5ed08bcfd11adc7746
EricR401S <eric.rios.3173@aol.com>
2023-06-08 16:56:25 -0400

    feature tagging progressing, prompting complete

--- feature_tagger.py
+++ feature_tagger.py
@@ -5,3 +5,38 @@ import pandas as pd
 
 
 user_tag_coordinates = pd.read_csv("tags.csv")
+
+# assuming a group reference image 
+# or a participant reference image
+# is received, this code should work agnostically
+
+
+participant_reference_gaze_csv = pd.read_csv("test reference gaze updated.csv")
+
+participant_reference_gaze_csv['ref_coordinates'] = "(" + participant_reference_gaze_csv['ref_x_pixel'].astype(str) + ',' + participant_reference_gaze_csv['ref_y_pixel'].astype(str) + ")"
+
+def ref_coordinate_processing(gaze_reference_df):
+
+    gaze_reference_df['ref_coordinates'] = pd.Series(zip(participant_reference_gaze_csv['ref_x_pixel'], participant_reference_gaze_csv['ref_y_pixel']))
+
+    # add assert statements here
+
+
+def gaze_in_box(gaze_reference_df_obs, tags_df):
+
+    tag_list_from_df = user_tag_coordinates.values.tolist()
+
+    # observation from that updated gaze csv
+
+    name = "noise"
+
+    smallest_center = None
+
+    gaze_x = gaze_reference_df_obs["ref_x_pixel"]
+    gaze_y = gaze_reference_df_obs["ref_y_pixel"]
+
+    for row in tag_list_from_df:
+
+        # 
+
+    
--- tagging_event.py
+++ tagging_event.py
@@ -29,25 +29,39 @@ def drawfunction(event, x, y, flags, param):
         )
         x2 = x
         y2 = y
+        center_x = int((x1 + x2) / 2)
+        center_y = int((y1 + y2) / 2)
         # left upper corner is (x1, y1)
         # right lower corner is (x, y)
         if x1 < x2 and y1 > y2:
-            feature_coordinates.extend([[name, x1, y1, x2, y2, x1, y2, x2, y1]])
+            # feature_coordinates.extend([[name, x1, y1, x2, y2, x1, y2, x2, y1]])
+            feature_coordinates.extend(
+                [[name, (x1, y1), (x2, y2), (x1, y2), (x2, y1), (center_x, center_y)]]
+            )
 
         # first coordinate is the lower right corner
         # second coordinate is the upper left corner
         elif x1 > x2 and y1 < y2:
-            feature_coordinates.extend([[name, x2, y2, x1, y1, x2, y1, x1, y2]])
+            # feature_coordinates.extend([[name, x2, y2, x1, y1, x2, y1, x1, y2]])
+            feature_coordinates.extend(
+                [[name, (x2, y2), (x1, y1), (x2, y1), (x1, y2), (center_x, center_y)]]
+            )
 
         # first coordinate is the lower left corner
         # second coordinate is the upper right corner
         elif x1 < x2 and y1 < y2:
-            feature_coordinates.extend([[name, x1, y2, x2, y1, x1, y1, x2, y2]])
+            # feature_coordinates.extend([[name, x1, y2, x2, y1, x1, y1, x2, y2]])
+            feature_coordinates.extend(
+                [[name, (x1, y2), (x2, y1), (x1, y1), (x2, y2), (center_x, center_y)]]
+            )
 
         # first coordinate is the upper right corner
         # second coordinate is the lower left corner
         elif x1 > x2 and y1 > y2:
-            feature_coordinates.extend([[name, x2, y1, x1, y2, x2, y2, x1, y1]])
+            # feature_coordinates.extend([[name, x2, y1, x1, y2, x2, y2, x1, y1]])
+            feature_coordinates.extend(
+                [[name, (x2, y1), (x1, y2), (x2, y2), (x1, y1), (center_x, center_y)]]
+            )
 
 
 # base_img = cv2.imread("test2 image prompter.png")
@@ -87,10 +101,43 @@ cv2.destroyAllWindows()
 # x2 and y2 correspond to the lower right corner
 # x3 and y3 correspond to the lower left corner
 # x4 and y4 correspond to the upper right corner
+# coordinates_df = pd.DataFrame(
+#     feature_coordinates,
+#     columns=["name", "x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4"],
+# )
+
 coordinates_df = pd.DataFrame(
     feature_coordinates,
-    columns=["name", "x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4"],
+    columns=["name", "(x1,y1)", "(x2,y2)", "(x3,y3)", "(x4,y4)", "(center_x,center_y)"],
 )
+
+# If all coordinates were locked in correctly
+# then the following assertions should pass
+# assert sum(coordinates_df["x1"] == coordinates_df["x3"]) == 4
+# assert sum(coordinates_df["x2"] == coordinates_df["x4"]) == 4
+# assert sum(coordinates_df["y1"] == coordinates_df["y4"]) == 4
+# assert sum(coordinates_df["y2"] == coordinates_df["y3"]) == 4
+
+assert [i[0] for i in coordinates_df["(x1,y1)"]] == [
+    i[0] for i in coordinates_df["(x3,y3)"]
+]
+assert [i[0] for i in coordinates_df["(x2,y2)"]] == [
+    i[0] for i in coordinates_df["(x4,y4)"]
+]
+assert [i[1] for i in coordinates_df["(x1,y1)"]] == [
+    i[1] for i in coordinates_df["(x4,y4)"]
+]
+assert [i[1] for i in coordinates_df["(x2,y2)"]] == [
+    i[1] for i in coordinates_df["(x3,y3)"]
+]
+
+print("assertions passed")
+
+# calculating the center of those rectangles
+# coordinates_df["center_x"] = (user_tag_coordinates['x1'] + user_tag_coordinates['x2'])/2
+# coordinates_df['center_x'] = coordinates_df['center_x'].astype("int64")
+# coordinates_df["center_y"] = (user_tag_coordinates['y1'] + user_tag_coordinates['y2'])/2
+# coordinates_df["center_y"] = coordinates_df["center_y"].astype("int64")
 
 print(coordinates_df)
 
