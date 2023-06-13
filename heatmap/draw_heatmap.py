54e3055a733392b6e6176d9c7cbce1d20a2db110
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2023-06-13 20:04:46 +0000

    updating heatmap code

--- heatmap/draw_heatmap.py
+++ heatmap/draw_heatmap.py
@@ -1,7 +1,9 @@
 """
 Author: Aditya John (aj391)
-This is a script that ingests a csv of gazes (when looking at an object) and outputs a final image with a heatmap across the various pixels of interest. 
-It also updates the gaze.csv file with new 'gaze' pixel locations that correspond to the reference image instead of the actual video. 
+This is a script that ingests a csv of gazes (when looking at an object) and outputs a final image 
+with a heatmap across the various pixels of interest. 
+It also updates the gaze.csv file with new 'gaze' pixel locations that correspond to the reference image 
+instead of the actual video. 
 
 # ToDo:
 - Add smoothing to the video file [pixel closeness? frame combination?]
@@ -10,67 +12,72 @@ It also updates the gaze.csv file with new 'gaze' pixel locations that correspon
 Ref: https://stackoverflow.com/questions/56472024/how-to-change-the-opacity-of-boxes-cv2-rectangle
 - QC the outputs - April?
 """
-import sys
 from collections import defaultdict
 import pandas as pd
 import cv2
-from functions import convert_timestamp_ns_to_ms, get_closest_individual_gaze_object, get_closest_reference_pixel, normalize_heatmap_dict, draw_heatmap_on_ref_img, create_output_directory
+from functions import (
+    convert_timestamp_ns_to_ms,
+    get_closest_individual_gaze_object,
+    get_closest_reference_pixel,
+    normalize_heatmap_dict,
+    draw_heatmap_on_ref_img
+)
 
+root_path = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/"
+folder_files = ["2022_03bm/2022_03bm", "2022_30b/2022_30b", "2022_39bm/2022_39bm"]
 
 for folder in folder_files:
-    print(f'Running for folder -- {folder}')
+    print(f"Running for folder -- {folder}")
     pixel_heatmap = defaultdict(int)
-    frame_no = 0folder_files = ['2022_03bm/2022_03bm', '2022_30b/2022_30b', '2022_39bm/2022_39bm']
-    #ToDo: Write a function to skip the first n frames based on percentage of grey/black color in the image
-    skip_first_n_frames = 60 # as the video starts with grey 
-    run_for_frames = 100 # Too low a value will cause a division by zero error
-    bounding_size = 25
-    name = folder.split('/')[0]
-    csv_file = f'/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/{folder}/fixations.csv'
-    video_file = f'/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/{folder}/video.mp4'
+    frame_no = 0
+    # ToDo: Write a function to skip the first n frames based on percentage of grey/black color in the image
+    skip_first_n_frames = 60  # as the video starts with grey
+    run_for_frames = 100  # Too low a value will cause a division by zero error
+    bounding_size = 250
+    name = folder.split("/", maxsplit=1)[0]
+    csv_file = f"{root_path}/{folder}/gaze.csv"
+    video_file = f"{root_path}/{folder}/video.mp4"
     gaze_csv = pd.read_csv(csv_file)
-    #gaze_csv = convert_timestamp_ns_to_ms(gaze_csv)
-    gaze_csv = convert_timestamp_ns_to_ms(gaze_csv, 'start timestamp [ns]')
-    gaze_csv = convert_timestamp_ns_to_ms(gaze_csv, 'end timestamp [ns]')
-    gaze_csv.to_csv('updated_fixation.csv', index=False)
+    gaze_csv = convert_timestamp_ns_to_ms(gaze_csv)
 
     updated_gaze = pd.DataFrame()
     cap = cv2.VideoCapture(video_file)
 
-    create_output_directory()
-    while(cap.isOpened()):
+    while cap.isOpened():
         if frame_no % 1000 == 0:
-            print(f'Processed {frame_no} frames')
+            print(f"Processed {frame_no} frames")
         frame_no += 1
         frame_exists, curr_frame = cap.read()
-        current_timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-        print(current_timestamp)
-        continue
+
         if frame_no < skip_first_n_frames:
             continue
 
         ##### Uncomment below if early stopping is required
         # if frame_no > skip_first_n_frames + run_for_frames:
-        #    break
-        
+        #     break
+
         elif frame_no == skip_first_n_frames and frame_exists:
             first_frame = curr_frame
-            cv2.imwrite(f'output/reference_image_{name}.png', first_frame)
+            cv2.imwrite(f"{root_path}/{folder}/reference_image_{name}.png", first_frame)
 
         elif frame_exists:
-            gaze_object_crop, closest_row = get_closest_individual_gaze_object(cap, curr_frame, gaze_csv, bounding_size)
-            closest_ref_pixel = get_closest_reference_pixel(first_frame, gaze_object_crop)
-            closest_row['ref_x_pixel'] = closest_ref_pixel[0]
-            closest_row['ref_y_pixel'] = closest_ref_pixel[1]
+            gaze_object_crop, closest_row = get_closest_individual_gaze_object(
+                cap, curr_frame, gaze_csv, bounding_size
+            )
+            ref_center = get_closest_reference_pixel(
+                first_frame, gaze_object_crop
+            )
+            closest_row["ref_center_x"] = ref_center[0]
+            closest_row["ref_center_y"] = ref_center[1]
             updated_gaze = pd.concat([updated_gaze, closest_row])
-            pixel_heatmap[closest_ref_pixel] += 1
+            pixel_heatmap[ref_center] += 1
 
         else:
             break
-    print(pixel_heatmap[0, 0])
+
     normalized_heatmap_dict = normalize_heatmap_dict(pixel_heatmap)
     final_img = draw_heatmap_on_ref_img(pixel_heatmap, first_frame, bounding_size)
 
-    cv2.imwrite(f'output/heatmap_output_{name}.png', final_img)
-    updated_gaze.to_csv(f'output/updated_gaze_{name}.csv', index=False)
+    cv2.imwrite(f"{root_path}/{folder}/heatmap_output_{name}.png", final_img)
+    updated_gaze.to_csv(f"{root_path}/{folder}/updated_gaze_{name}.csv", index=False)
     cap.release()
--- heatmap/functions.py
+++ heatmap/functions.py
@@ -2,106 +2,135 @@
 Author: Aditya John (aj391)
 This script contains helper functions used by the draw_heatmap.py script
 """
-import os
+import sys
+import random
 import cv2
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-def convert_timestamp_ns_to_ms(gaze_df, col_name='timestamp [ns]', subtract=False):
+def convert_timestamp_ns_to_ms(gaze_df, col_name="timestamp [ns]", subtract=False):
     """
     Simple function to convert the ns linux timestamp datetype to normal milliseconds of elapsed time
     """
     gaze_df[col_name] = pd.to_datetime(gaze_df[col_name])
     start_timestamp = gaze_df[col_name][0]
     if subtract:
-        gaze_df[col_name] = (gaze_df[col_name] - start_timestamp)
+        gaze_df[col_name] = gaze_df[col_name] - start_timestamp
     gaze_df[col_name] = gaze_df[col_name].astype(np.int64) / int(1e6)
     return gaze_df
 
+
 def get_closest_individual_gaze_object(cap, curr_frame, gaze_csv, bounding_size):
     """
-    Function to look at the current timestamp and return the pixel locations in the gaze csv that is closest to it. 
-    Draws a bounding box of the specified size around that specific pixel location and returns the bounding box as a cropped image. 
+    Function to look at the current timestamp and return the pixel locations in the gaze csv that is closest to it.
+    Draws a bounding box of the specified size around that specific pixel location and returns the bounding box as a cropped image.
     """
     current_timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-    print(current_timestamp)
-    closet_value = min(gaze_csv['timestamp [ns]'], key=lambda x:abs(x-current_timestamp))
-    closest_row = pd.DataFrame(gaze_csv[gaze_csv['timestamp [ns]'] == closet_value].reset_index())
-    x_pixel = round(closest_row['gaze x [px]'][0])
-    y_pixel = round(closest_row['gaze x [px]'][0])
-    template = curr_frame[y_pixel:y_pixel+bounding_size, x_pixel:x_pixel+bounding_size]
+    closet_value = min(
+        gaze_csv["timestamp [ns]"], key=lambda x: abs(x - current_timestamp)
+    )
+    closest_row = pd.DataFrame(
+        gaze_csv[gaze_csv["timestamp [ns]"] == closet_value].reset_index()
+    )
+    x_pixel = round(closest_row["gaze x [px]"][0])
+    y_pixel = round(closest_row["gaze y [px]"][0])
+    template = curr_frame[
+        y_pixel : y_pixel + bounding_size, x_pixel : x_pixel + bounding_size
+    ]
     return template, closest_row
 
-def get_closest_reference_pixel(first_frame, template, method=1):
+
+def get_closest_reference_pixel(target, template, chosen_method=1):
     """
-    Compares the cropped image from the above function to the reference image (likely the first non-grey frame) 
+    Compares the cropped image from the above function to the reference image (likely the first non-grey frame)
     and returns the pixel locations that most closely matches it
     """
-    chosen_method = 1
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+    """
+    Using SIFT
+    sift = cv2.SIFT_create()
+
+    # Find keypoints and compute descriptors for the template and target images
+    keypoints_template, descriptors_template = sift.detectAndCompute(template, None)
+    keypoints_target, descriptors_target = sift.detectAndCompute(target, None)
+
+   # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(descriptors_template,descriptors_target,k=2)
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in range(len(matches))]
+    # ratio test as per Lowe's paper
+    for i,(m,n) in enumerate(matches):
+        if m.distance < 0.7*n.distance:
+            matchesMask[i]=[1,0]
+    draw_params = dict(matchColor = (0,255,0),
+                    singlePointColor = (255,0,0),
+                    matchesMask = matchesMask,
+                    flags = cv2.DrawMatchesFlags_DEFAULT)
+    img3 = cv2.drawMatchesKnn(template,keypoints_template,target,keypoints_target,matches,None,**draw_params)
+    plt.imshow(img3,),plt.show()
+    number = random.randint(0, 1000)
+    cv2.imwrite(f"trash/{number}_matched.png", img3)
+    cv2.imwrite(f"trash/{number}_template.png", template)
+    cv2.imwrite(f"trash/{number}_target.png", target)
+
+
+    return (0, 0)
+    """
+
+    ###### Uses template matching
+    methods = [
+        "cv2.TM_CCOEFF",
+        "cv2.TM_CCOEFF_NORMED",
+        "cv.TM_CCORR",
+        "cv2.TM_CCORR_NORMED",
+        "cv2.TM_SQDIFF",
+        "cv2.TM_SQDIFF_NORMED",
+    ]
+    w, h, _ = template.shape[::-1]
     method = eval(methods[chosen_method])
-    res = cv2.matchTemplate(first_frame, template, method)
+    res = cv2.matchTemplate(target, template, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
         top_left = min_loc
     else:
         top_left = max_loc
-    return top_left
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    center = (int((top_left[0] + bottom_right[1])/2), int((top_left[1] + bottom_right[1])/2))
+    return center
 
-def normalize_heatmap_dict(pixel_heatmap, new_min=0, new_max=1):
-    """
-    Function to normalize the heatmap dictionary counts between the given range
-    """
-    old_min = pixel_heatmap[min(pixel_heatmap, key=pixel_heatmap.get)]
-    old_max = pixel_heatmap[max(pixel_heatmap, key=pixel_heatmap.get)]
-    new_min = new_min
-    new_max = new_max
-    old_range = (old_max - old_min)  
-    new_range = (new_max - new_min) 
-    
-    for key, value in pixel_heatmap.items():
-        color = (((value - old_min) * new_range) / old_range) + new_min
-        pixel_heatmap[key] = color
 
-def draw_heatmap_on_ref_img(pixel_heatmap, first_frame, bounding_size):
+def normalize_heatmap_dict(pixel_heatmap):
+    EPSILON = sys.float_info.epsilon  # Smallest possible difference.
+    colors = [(0, 0, 255), (0, 255, 0),(255, 0, 0)]
+    minval = pixel_heatmap[min(pixel_heatmap, key=pixel_heatmap.get)]
+    maxval = pixel_heatmap[max(pixel_heatmap, key=pixel_heatmap.get)]
 
+    for key, val in pixel_heatmap.items():
+        i_f = float(val - minval) / float(maxval - minval) * (len(colors) - 1)
+        i, f = int(i_f // 1), i_f % 1
+        if f < EPSILON:
+            pixel_heatmap[key] = colors[i]
+        else:
+            (r1, g1, b1), (r2, g2, b2) = colors[i], colors[i + 1]
+            pixel_heatmap[key] = (
+                int(r1 + f * (r2 - r1)),
+                int(g1 + f * (g2 - g1)),
+                int(b1 + f * (b2 - b1)),
+            )
+    return pixel_heatmap
+
+
+def draw_heatmap_on_ref_img(pixel_heatmap, first_frame, bounding_size=3):
     """
     Function to draw the heatmap on the reference image based on the pixel locations
     """
-    """ #### Not working
-    overlay = first_frame.copy()
-    ### Translucent small circle bounding box
+    bounding_size = 10
     for key, value in pixel_heatmap.items():
-        bottom_right = (key[0] + bounding_size, key[1] + bounding_size)
-        #value = abs(255-value)
-        color = (255, 255, 255)
-        op_frame = cv2.circle(first_frame, key, 3, color, -1)
-        alpha = 1-value  # Transparency factor.
-        op_frame = cv2.addWeighted(overlay, alpha, op_frame, 1 - alpha, 0)
+        op_frame = cv2.circle(first_frame, key, bounding_size, value, 2)
     return op_frame
-    """
-
-
-    ### Small circle bounding box
-    for key, value in pixel_heatmap.items():
-        bottom_right = (key[0] + bounding_size, key[1] + bounding_size)
-        value = abs(255-value)
-        color = (value, value, 255)
-        op_frame = cv2.circle(first_frame, key, 3, color, 2)
-    return op_frame
-    
-
-    ### Rectangle bounding box version
-    """
-    for key, value in pixel_heatmap.items():
-        bottom_right = (key[0] + bounding_size, key[1] + bounding_size)
-        value = abs(255-value)
-        color = (value, value, 255)
-        op_frame = cv2.rectangle(first_frame, key, bottom_right, color, 2) 
-    return op_frame
-    """
-def create_output_directory():
-    if not os.path.exists('./output'):
-        os.makedirs('./output')
