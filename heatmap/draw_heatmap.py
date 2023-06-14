3f761dd3e852708e1966ea8c2dc55330310397b3
EricR401S <eric.rios.3173@aol.com>
2023-06-14 13:51:26 -0400

    making it run on local computer with Aditya here

--- heatmap/draw_heatmap.py
+++ heatmap/draw_heatmap.py
@@ -20,24 +20,37 @@ from functions import (
     get_closest_individual_gaze_object,
     get_closest_reference_pixel,
     normalize_heatmap_dict,
-    draw_heatmap_on_ref_img
+    draw_heatmap_on_ref_img,
 )
+import os
+import glob
 
-root_path = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/"
+# root_path = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/"
+# root_path = r"C:\Users\ericr\Desktop\Data + Plus\eye tracking data from the museum in Rome (Pupil Invisible)"
 folder_files = ["2022_03bm/2022_03bm", "2022_30b/2022_30b", "2022_39bm/2022_39bm"]
 
-for folder in folder_files:
+root_path = r"C:\Users\ericr\Desktop\Data + Plus\eye tracking data from the museum in Rome (Pupil Invisible)"
+
+
+# for folder in folder_files:
+for folder in os.listdir(root_path):
+    folder = os.path.join(root_path, folder)
     print(f"Running for folder -- {folder}")
     pixel_heatmap = defaultdict(int)
     frame_no = 0
-    
+
     # ToDo: Write a function to skip the first n frames based on percentage of grey/black color in the image
     skip_first_n_frames = 60  # as the video starts with grey
     run_for_frames = 100  # Too low a value will cause a division by zero error
     bounding_size = 250
-    name = folder.split("/", maxsplit=1)[0]
-    csv_file = f"{root_path}/{folder}/gaze.csv"
-    video_file = f"{root_path}/{folder}/video.mp4"
+    name = folder.split("\\")[-1]
+    print(f"The name of the participant is : {name}")
+    # csv_file = f"{root_path}/{folder}/gaze.csv"
+    csv_file = os.path.join(folder, "gaze.csv")
+    # video_file = f"{root_path}/{folder}/video.mp4"
+    video_file = os.path.join(folder, "*.mp4")
+    video_file = glob.glob(video_file)[0]
+    # print(glob.glob(video_file))
     gaze_csv = pd.read_csv(csv_file)
     gaze_csv = convert_timestamp_ns_to_ms(gaze_csv)
 
@@ -59,15 +72,14 @@ for folder in folder_files:
 
         elif frame_no == skip_first_n_frames and frame_exists:
             first_frame = curr_frame
-            cv2.imwrite(f"{root_path}/{folder}/reference_image_{name}.png", first_frame)
+            # cv2.imwrite(f"{root_path}/{folder}/reference_image_{name}.png", first_frame)
+            cv2.imwrite(f"output_heatmap/{name}_reference_image.png", first_frame)
 
         elif frame_exists:
             gaze_object_crop, closest_row = get_closest_individual_gaze_object(
                 cap, curr_frame, gaze_csv, bounding_size
             )
-            ref_center = get_closest_reference_pixel(
-                first_frame, gaze_object_crop
-            )
+            ref_center = get_closest_reference_pixel(first_frame, gaze_object_crop)
             closest_row["ref_center_x"] = ref_center[0]
             closest_row["ref_center_y"] = ref_center[1]
             updated_gaze = pd.concat([updated_gaze, closest_row])
@@ -79,6 +91,11 @@ for folder in folder_files:
     normalized_heatmap_dict = normalize_heatmap_dict(pixel_heatmap)
     final_img = draw_heatmap_on_ref_img(pixel_heatmap, first_frame, bounding_size)
 
-    cv2.imwrite(f"{root_path}/{folder}/heatmap_output_{name}_{bounding_size}.png", final_img)
-    updated_gaze.to_csv(f"{root_path}/{folder}/updated_gaze_{name}.csv", index=False)
+    # cv2.imwrite(
+    #    f"{root_path}/{folder}/heatmap_output_{name}_{bounding_size}.png", final_img
+    # )
+
+    cv2.imwrite(f"output_heatmap/{name}_heatmap.png", final_img)
+
+    # updated_gaze.to_csv(f"{root_path}/{folder}/updated_gaze_{name}.csv", index=False)
     cap.release()
