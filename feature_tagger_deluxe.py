d777dd2e412aca62cfcd45bb0115d7bdf0243537
EricR401S <eric.rios.3173@aol.com>
2023-06-16 12:31:44 -0400

    fixing the tagger still for all participants

--- feature_tagger_deluxe.py
+++ feature_tagger_deluxe.py
@@ -3,6 +3,8 @@ from repository_finder import repository_details
 import os
 from tag_event_functions import drawfunction
 import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
 
 ROOT_PATH, ART_PIECE = repository_details("Paths.txt")
 
@@ -20,17 +22,78 @@ for folder in os.listdir(ROOT_PATH):
         print(f"{folder} is a file")
         continue
 
-for folder in participant_paths_folders:
+from repository_finder import repository_details
+import os
+from tag_event_functions import drawfunction
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+
+for folder in participant_paths_folders[0:2]:
     files = os.listdir(folder)
+    participant_id = folder
+    feature_coordinates = []
+    drawing = True
+    flag = True
 
     for single_file in files:
-        if ".mp4" in single_file:
+        if "reference_image" in single_file:
             file = os.path.join(folder, single_file)
             print(f"Running for file -- {single_file}")
-    print(f"file is {file}")
+
     base_img = cv2.imread(file)
+
     # base_img = cv2.imread("test4 image prompter.jpg")
     # img = cv2.cvtColor(base_img, cv2.COLOR_BGR2RGB)
     img = base_img
     reset_img = img.copy()
-    plt.imshow(img)
+    # plt.imshow(img)
+
+    cv2.namedWindow("image")
+
+    cv2.setMouseCallback("image", drawfunction)
+
+    while flag:
+        cv2.imshow("image", img)
+        key = cv2.waitKey(1)
+        if key == ord("0"):
+            break
+
+        elif key == ord("5"):
+            img = reset_img.copy()
+            cv2.imshow("image", img)
+            print("You have reset the image")
+
+        elif key == ord("9"):
+            flag = False
+            print("You have finished tagging")
+        # cv2.imshow("image", img)
+
+    cv2.destroyAllWindows()
+
+    coordinates_df = pd.DataFrame(
+        feature_coordinates,
+        columns=[
+            "name",
+            "(x1,y1)",
+            "(x2,y2)",
+            "(x3,y3)",
+            "(x4,y4)",
+            "(center_x,center_y)",
+        ],
+    )
+
+    assert [i[0] for i in coordinates_df["(x1,y1)"]] == [
+        i[0] for i in coordinates_df["(x3,y3)"]
+    ]
+    assert [i[0] for i in coordinates_df["(x2,y2)"]] == [
+        i[0] for i in coordinates_df["(x4,y4)"]
+    ]
+    assert [i[1] for i in coordinates_df["(x1,y1)"]] == [
+        i[1] for i in coordinates_df["(x4,y4)"]
+    ]
+    assert [i[1] for i in coordinates_df["(x2,y2)"]] == [
+        i[1] for i in coordinates_df["(x3,y3)"]
+    ]
+
+    print("assertions passed")
