053dbbedf0bd2ef5e8ed1b3c38541cafa81a46c5
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2023-06-20 17:29:48 +0000

    added config file

---
+++ heatmap/config.py
@@ -0,0 +1,17 @@
+import os
+
+class base_class():
+    SKIP_FIRST_N_FRAMES = 60  # As some (most) videos start with grey screen
+    RUN_FOR_FRAMES = 100  # Too low a value will cause a division by zero error
+    DETECT_BOUNDING_SIZE = 50  # Size of the bounding box for detecition
+    DRAW_BOUNDING_SIZE = 3  # Radius of the circle for the bounding box on the heatmap
+    RESAMPLE = False  # Resample (from ns to ms) or choose the closest row
+
+class aditya_config(base_class):
+    ROOT_PATH = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/data"
+    TEMP_OUTPUT_DIR = "." + os.sep + "output"
+
+class eric_config(base_class):
+    ROOT_PATH = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/data"
+    TEMP_OUTPUT_DIR = "." + os.sep + "output"
+
--- heatmap/draw_heatmap.py
+++ heatmap/draw_heatmap.py
@@ -13,9 +13,9 @@ Ref: https://stackoverflow.com/questions/56472024/how-to-change-the-opacity-of-b
 - QC the outputs - April?
 """
 from collections import defaultdict
-import traceback
 import os
 import glob
+import sys
 import pandas as pd
 import numpy as np
 import cv2
@@ -29,21 +29,20 @@ from functions import (
     resample_gaze,
     save_outputs,
 )
+from config import *
 
-# Define Constant Variables
-SKIP_FIRST_N_FRAMES = 60  # As some (most) videos start with grey screen
-RUN_FOR_FRAMES = 100  # Too low a value will cause a division by zero error
-DETECT_BOUNDING_SIZE = 50  # Size of the bounding box for detecition
-DRAW_BOUNDING_SIZE = 3  # Radius of the circle for the bounding box on the heatmap
-RESAMPLE = False  # Resample (from ns to ms) or choose the closest row
-RESAMPLE = False  # Resample (from ns to ms) or choose the closest row
-ROOT_PATH = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/data"
-ROOT_PATH = r"C:\Users\ericr\Desktop\Data + Plus\eye tracking data from the museum in Rome (Pupil Invisible)"
-TEMP_OUTPUT_DIR = "." + os.sep + "output"
-create_directory(TEMP_OUTPUT_DIR)
+# Set env variables based on config file
+try:
+    env = sys.argv[1]
+    env_var = eval(env+'_config')
+except:
+    print('Enter valid env variable. Refer to classes in the config.py file')
+    sys.exit()
 
-for index, folder in enumerate(os.listdir(ROOT_PATH)):
-    folder = os.path.join(ROOT_PATH, folder)
+create_directory(env_var.TEMP_OUTPUT_DIR)
+
+for index, folder in enumerate(os.listdir(env_var.ROOT_PATH)):
+    folder = os.path.join(env_var.ROOT_PATH, folder)
     print("#" * 50)
     print(f"Running for folder {index} -- {folder}")
     pixel_heatmap = defaultdict(int)
@@ -54,12 +53,11 @@ for index, folder in enumerate(os.listdir(ROOT_PATH)):
 
     csv_file = os.path.join(folder, "gaze.csv")
     video_file = os.path.join(folder, "*.mp4")
-    print(f"Running for video file -- {video_file}")
     video_file = glob.glob(video_file)[0]
-    print(f"Running for video file -- {video_file}")
+
     gaze_df = pd.read_csv(csv_file)
     gaze_df = convert_timestamp_ns_to_ms(gaze_df)
-    if RESAMPLE:
+    if env_var.RESAMPLE:
         gaze_df = resample_gaze(gaze_df)
 
     updated_gaze = pd.DataFrame()
@@ -72,19 +70,19 @@ for index, folder in enumerate(os.listdir(ROOT_PATH)):
         frame_no += 1
         frame_exists, curr_frame = cap.read()
 
-        if frame_no < SKIP_FIRST_N_FRAMES:
+        if frame_no < env_var.SKIP_FIRST_N_FRAMES:
             continue
 
         ##### Uncomment below if early stopping is required
         # if frame_no > SKIP_FIRST_N_FRAMES + RUN_FOR_FRAMES:
         #    break
 
-        elif frame_no == SKIP_FIRST_N_FRAMES and frame_exists:
+        elif frame_no == env_var.SKIP_FIRST_N_FRAMES and frame_exists:
             first_frame = curr_frame
 
         elif frame_exists:
             gaze_object_crop, closest_row = get_closest_individual_gaze_object(
-                cap, curr_frame, gaze_df, DETECT_BOUNDING_SIZE
+                cap, curr_frame, gaze_df, env_var.DETECT_BOUNDING_SIZE
             )
 
             if not gaze_object_crop.any() or closest_row.empty: 
@@ -99,8 +97,7 @@ for index, folder in enumerate(os.listdir(ROOT_PATH)):
             updated_gaze = pd.concat([updated_gaze, closest_row])
             pixel_heatmap[ref_center] += 1
 
-            # Below code is just for plotting the centre of the images
-
+            #####  Below code is just for plotting the centre of the images
             # _x = int(closest_row['gaze x [px]'].iloc[0])
             # _y = int(closest_row['gaze y [px]'].iloc[0])
             # pixel_heatmap[_x, _y] += 1
@@ -110,27 +107,18 @@ for index, folder in enumerate(os.listdir(ROOT_PATH)):
 
     normalized_heatmap_dict = normalize_heatmap_dict(pixel_heatmap)
     final_img = draw_heatmap_on_ref_img(
-        pixel_heatmap, np.copy(first_frame), DRAW_BOUNDING_SIZE
+        pixel_heatmap, np.copy(first_frame), env_var.DRAW_BOUNDING_SIZE
     )
 
     cap.release()
     save_outputs(
-        ROOT_PATH,
+        env_var.ROOT_PATH,
         name,
         first_frame,
-        DETECT_BOUNDING_SIZE,
+        env_var.DETECT_BOUNDING_SIZE,
         final_img,
         updated_gaze,
-        TEMP_OUTPUT_DIR,
+        env_var.TEMP_OUTPUT_DIR,
     )
-
-        ### Write the data to the temp output folder
-        cv2.imwrite(f"{TEMP_OUTPUT_DIR}/{name}_reference_image.png", first_frame)
-        cv2.imwrite(f"{TEMP_OUTPUT_DIR}/{name}_heatmap.png", final_img)
-        updated_gaze.to_csv(f"{TEMP_OUTPUT_DIR}/{name}_updated_gaze.csv", index=False)
-    except Exception as ee:
-        print(frame_no)
-        print(ee)
-        continue
 
 
