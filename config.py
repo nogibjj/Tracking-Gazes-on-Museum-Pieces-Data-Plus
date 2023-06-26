5d2ed878433e083fb6ad9c082ece1babe08bf4c3
EricR401S <eric.rios.3173@aol.com>
2023-06-26 13:25:03 -0400

    updated config

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ class base_class:
     DRAW_BOUNDING_SIZE = 3  # Radius of the circle for the bounding box on the heatmap
     RESAMPLE = False  # Resample (from ns to ms) or choose the closest row
 
+    # PLACE THE NAME OF YOUR ART PIECE AFTER THE ART PIECE VARIABLE
+    # IF THERE IS MORE THAN ONE ART PIECE OR OBJECT OF INTEREST IN THE VIDEO
+    # TURN THE OBJECT OF INTEREST INTO A LIST LIKE ["ART PIECE 1", "ART PIECE 2"]
+    ART_PIECE = ["Truscan Couple Statue"]
+
 
 class aditya_config(base_class):
     ROOT_PATH = "/workspaces/Tracking-Gazes-on-Museum-Pieces-Data-Plus/data"
@@ -21,4 +26,19 @@ class eric_config(base_class):
 
 class april_config(base_class):
     ROOT_PATH = "/Users/aprilzuo/Downloads/eye tracking data from the museum in Rome (Pupil Invisible)"
+    TEMP_OUTPUT_DIR = "." + os.sep + "output"
+
+
+# INSTRUCTIONS
+
+# These instructions assume you have already downloaded the data
+# and divided it into the folders of your participants
+# In addition, we expect the folders to contain a csv file with the gaze coordinates and a video mp4 file.
+
+
+class user_config(base_class):
+    # Place your path with all of your participant folders after the ROOT_PATH variable
+    # Erase "None" and put your path in quotes like this r"PATH"
+    ROOT_PATH = None
+    #  C:\Users\ericr\Desktop\Data + Plus\eye tracking data from the museum in Rome (Pupil Invisible)
     TEMP_OUTPUT_DIR = "." + os.sep + "output"
