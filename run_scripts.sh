342e2761004f9c2932f9a68acef99e9d914512f2
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2023-07-26 16:24:01 +0000

    updated run_script.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-
 read -p 'Enter the user whose configuration needs to be used for the script: ' user
-# Change to the directory containing the Python script
-cd heatmap
-# python draw_heatmap.py $user
 
+
+# Run heatmap code
+cd heatmap
+python draw_heatmap.py $user
+
+
+### Run scanpath visualizations
 cd ..
 cd scanpath_visualization
 python ref_image_plotting.py $user
 python video_plotting.py $user
 
+
+### Run tagger
 cd ..
 cd tagging_tool
-python feature_tagger_deluxe.py
+python feature_tagger_deluxe.py $user
+
+### Run grouper
+
+### Run analysis script
 
