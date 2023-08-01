56711806f081ba4f0f1d37d17e71d3b6d29d7053
EricR401S <eric.rios.3173@aol.com>
2023-08-01 14:09:56 -0400

    bash script update

--- run_scripts.sh
+++ run_scripts.sh
@@ -4,27 +4,24 @@ pip install -r requirements.txt
 read -p 'Enter the user whose configuration needs to be used for the script: ' user
 
 # Create the reference image and tag it
-cd heatmap
-python reference_image_creator.py $user
+cd create_reference_image
+python main.py $user
 cd ../tagging_tool
-python feature_tagger.py $user
+python main.py $user
 
 # Create heatmaps and map the data from video to reference image
 cd ../heatmap
-python draw_heatmap.py $user
+python main.py $user
 
 # Run the Scanpath Visualisations
 cd ../scanpath_visualization
 python ref_image_plotting.py $user
 python video_plotting.py $user
 
-### Run tag
-cd ../tagging_tool
-python gaze_tag_mapper.py $user
-
-# Run grouper
-python grouping_csvs.py $user
+### Run mapper and grouper script
+cd ../mapping_tool
+python main.py $user
 
 # Run analysis script
 cd ../analysis
-python analysis.py $user
+python main.py $user
